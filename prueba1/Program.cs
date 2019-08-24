29b9dc2655ea4dbf4fa54fe7646a3e3e0b5def82
gustavoetcheverry <gustavo.etcheverry@gmail.com>
2019-08-23 20:34:35 -0400

    quinto

--- prueba1/Program.cs
+++ prueba1/Program.cs
@@ -14,6 +14,7 @@ namespace prueba1
             Console.WriteLine("segundo cambio");
             Console.WriteLine("tecrcer cambio");
             Console.WriteLine("cuarto cambio");
+            Console.WriteLine("quinto cambio");
             ;        }
     }
 }
